07ea421a0db4be4c9ac5bef1ab414d393956cd2c
Eric Champlin <Eric@Erics-MacBook-Pro.local>
2024-11-18 13:07:04 -0600

    updated pages css

--- src/pages/RecipeDetails.tsx
+++ src/pages/RecipeDetails.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import NavBar from '../components/NavBar.tsx';
-
+import './RecipeDetails.css';
 
 const RecipeDetails = () => {
-    return (
-      <div>
-        <NavBar />
-        <h1>Recipe Name</h1>
-        <p>Ingredients: ...</p>
-        <p>Directions: ...</p>
-        <button>Save Recipe</button>
+  return (
+    <div className="create-recipe-container">
+      <NavBar />
+      <h1 className="recipe-name">Recipe Name</h1>
+      <img 
+        src="https://via.placeholder.com/350" 
+        alt="Recipe" 
+        className="recipe-image"
+      />
+      <div className="ingredient-list">
+        <p className="ingredient-item" style={{ backgroundColor: '#FFD966' }}>Ingredients</p>
+        <p className="ingredient-item" style={{ backgroundColor: '#B4B4B4' }}>Ingredients</p>
+        <p className="ingredient-item" style={{ backgroundColor: '#FFD966' }}>Ingredients</p>
+        <p className="ingredient-item" style={{ backgroundColor: '#B4B4B4' }}>Ingredients</p>
+        <p className="ingredient-item" style={{ backgroundColor: '#FFD966' }}>Ingredients</p>
       </div>
-    );
-  };
-  
-  export default RecipeDetails;
+      <button className="save-recipe-button">Save Recipe</button>
+    </div>
+  );
+};
+
+export default RecipeDetails;
