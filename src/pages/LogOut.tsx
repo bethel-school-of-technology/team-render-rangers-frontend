5e21c1b77ad63a0e284c8ce8f5d83076889172af
Eric Champlin <Eric@Erics-MacBook-Pro.local>
2024-11-27 06:14:19 -0600

    edited logout popup

--- src/pages/LogOut.tsx
+++ src/pages/LogOut.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import './LogOut.css';
-import NavBar from '../components/NavBar.tsx';
+// import React from 'react';
+// import './LogOut.css';
+// import NavBar from '../components/NavBar.tsx';
 
-const LogOut = () => {
-  const handleLogout = () => {
-    // Handle logout functionality here
-  };
+// const LogOut = () => {
+//   const handleLogout = () => {
+//     // Handle logout functionality here
+//   };
 
-  return (
-    <div>
-      <NavBar />
-      <button onClick={handleLogout} className="logout-button">Log Out</button>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <NavBar />
+//       <button onClick={handleLogout} className="logout-button">Log Out</button>
+//     </div>
+//   );
+// };
 
-export default LogOut;
+// export default LogOut;
