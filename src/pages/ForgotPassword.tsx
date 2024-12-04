e4c84cd9a9e404425e53a910743093d819236a64
Eric Champlin <Eric@Erics-MBP.attlocal.net>
2024-12-04 17:55:12 -0600

    updated css
    updated ForgotPassword css

---
+++ src/pages/ForgotPassword.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './ForgotPassword.css';
+
+const ForgotPassword = () => {
+    return (
+        <div className="forgot-password-container">
+            <form className="forgot-password-form">
+                <img 
+                    src="https://foodtech-files.s3-eu-west-2.amazonaws.com/1dfbb06d9f9eb0d475d8559b4176078863993447b0b1fa758cbb476b2867c73d" 
+                    alt="Feastly Logo" 
+                    className="feastly-logo" 
+                />
+                <input 
+                    type="email" 
+                    name="email" 
+                    placeholder="Email" 
+                    required 
+                    className="forgot-password-input"
+                />
+                <button type="submit" className="forgot-password-button">
+                    Send Reset Link
+                </button>
+                <a href="/" className="login-link">Back to Log In</a>
+            </form>
+        </div>
+    );
+};
+
+export default ForgotPassword;
